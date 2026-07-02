9f9889a1a8edf0d998eb421d7e50ac75659e7980
Rajan KC <rajankckc7@gmail.com>
2026-07-02 10:43:09 +0000

    chore: initialize project with package.json and dependencies

---
+++ index.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+
+const axios = require("axios");
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
+
+app.command("/cybro-help", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text: `Available Commands:
+/cybro-help - Command List
+/cybro-ping - Check bot latency
+/cybro-catfact - Get a cat fact
+/cybro-joke - Get a Joke
+/cybro-ask - Ask anything
+/cybro-info - Infromation about Cybro`,
+  });
+});
+
+app.command("/cybro-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+app.command("/cybro-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+app.command("/cybro-joke", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get(
+      "https://official-joke-api.appspot.com/random_joke",
+    );
+    await respond({
+      text: `${response.data.setup}
+${response.data.punchline}`,
+    });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a joke." });
+  }
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
